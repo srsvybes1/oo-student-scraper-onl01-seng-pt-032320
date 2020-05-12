426d18700ac4c3f5c397d5c4056d0801b500dd5f
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-12 23:13:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each {|student| student.new(Student)}
+    students_array.each {|student| Student.new(Student)}
   end
 
   def add_student_attributes(attributes_hash)
